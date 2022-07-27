905fd6deb537cef1d68f8028b3dcdc2802e9ae7e
Camilo Salamanca <casc6798-@hotmail.com>
2022-07-26 20:48:44 -0500

    Cambio/optimizacion en funciones, limpieza de codigo. Se creo la funcion getElementById, getElementValueById, addNewRowToTable y la funcion doOnClick.

--- Pagina salud/main.js
+++ Pagina salud/main.js
@@ -1,60 +1,85 @@
 window.onload = () => {
 
-    document.getElementById("id_mandalo").onclick = () => {
-        console.log(document.getElementById("id_nombre").value);
-        console.log(document.getElementById("id_apellido").value);
-        console.log(document.getElementById("id_correo").value);
-        console.log(document.getElementById("id_numero").value);
-        console.log(document.getElementById("id_nacionalidad").value);
-        console.log(document.getElementById("id_ocupacion").value);
 
-        document.getElementById("id_nombre").value;
+    const idInputEnviar = "enviar";
+    const idInputNombre = "nombre";
+    const idInputApellido = "apellido";
+    const idInputCorreo = "correo";
+    const idInputNumero = "numero";
+    const idInputNacionalidad = "nacionalidad";
+    const idInputOcupacion = "ocupacion"; 
+    const prefitsIdInput = "id_";
+    const idTabla = "tbody";
 
-        function funcionclass () {
-
-        }
-
-        var datos = {
-            id_nombre: document.getElementById("id_nombre").value,
-            id_apellido: document.getElementById("id_apellido").value,
-            id_correo: document.getElementById("id_correo").value,
-            id_numero: document.getElementById("id_numero").value,
-            id_nacionalidad: document.getElementById("id_nacionalidad").value,
-            id_ocupacion: document.getElementById("id_ocupacion").value,
-        }
-
-        funcioncamilo(datos)
+    getElementById(idInputEnviar).onclick = () => {
+        doOnClick ();
     };
+
+    function getElementById (elementId) {
+        let elemento = document.getElementById(prefitsIdInput + elementId)
+        return elemento
+    }
+
+    function getElementValueById (elementId) {
+        let elementValue = getElementById (elementId);
+        return elementValue.value
+    }
+
+    function consoleLogByElementId (elementId) {
+        console.log(getElementById(elementId));
+    }
+
+
+    function doOnClick () {
+        // consoleLogByElementId(idInputNombre);
+        // consoleLogByElementId(idInputApellido);
+        // consoleLogByElementId(idInputCorreo);
+        // consoleLogByElementId(idInputNumero);
+        // consoleLogByElementId(idInputNacionalidad);
+        // consoleLogByElementId(idInputOcupacion);
+        var datos = {
+            nombre: getElementValueById(idInputNombre),
+            apellido: getElementValueById(idInputApellido),
+            correo: getElementValueById(idInputCorreo),
+            numero: getElementValueById(idInputNumero),
+            nacionalidad: getElementValueById(idInputNacionalidad),
+            ocupacion: getElementValueById(idInputOcupacion),
+        }
+
+        addNewRowToTable(datos)
+    }
     
-    function funcioncamilo(datos) {
-        var table = document.getElementById("id_tbody");
+    function addCellWithValue (index, value, row) {
+        var cell = row.insertCell(index);
+        cell.innerHTML = value;
+    }
+
+    function addNewRowToTable (datos) {
+        
+        
+        var table = getElementById("tbody");
         var row = table.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
-        cell1.innerHTML = datos.id_nombre;
-        cell2.innerHTML = datos.id_apellido;
-        cell3.innerHTML = datos.id_correo;
-        cell4.innerHTML = datos.id_numero;
-        cell5.innerHTML = datos.id_nacionalidad;
-        cell6.innerHTML = datos.id_ocupacion;
+
+        addCellWithValue (0, datos.nombre, row)
+        addCellWithValue (1, datos.apellido, row)
+        addCellWithValue (2, datos.correo, row)
+        addCellWithValue (3, datos.numero, row)
+        addCellWithValue (4, datos.nacionalidad, row)
+        addCellWithValue (5, datos.ocupacion, row)
     }
 
 
     document.getElementById("id_autocompletar").onclick = () => {
-        mifuncion()
+        miFuncion()
     };
 
-    function mifuncion() {
-        document.getElementById("id_nombre").value = "Camilo";
-        document.getElementById("id_apellido").value = "Salamanca";
-        document.getElementById("id_correo").value = "casc@hotdog.com";
-        document.getElementById("id_numero").value = "3223223223";
-        document.getElementById("id_nacionalidad").value = "Colombiano";
-        document.getElementById("id_ocupacion").value = "Chef";
+    function miFuncion() {
+        getElementById(idInputNombre).value = "Camilo";
+        getElementById(idInputApellido).value = "Salamanca";
+        getElementById(idInputCorreo).value = "casc@hotdog.com";
+        getElementById(idInputNumero).value = "3223223223";
+        getElementById(idInputNacionalidad).value = "Colombiano";
+        getElementById(idInputOcupacion).value = "Chef";
     }
 
     // document.getElementById("id_nombre").value
